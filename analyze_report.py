e233857c4fb38198260bcc291ff62d8cb924f210
Ignacio Laguna <ilaguna@llnl.gov>
2019-08-22 15:39:32 -0700

    new analysis

--- analyze_report.py
+++ analyze_report.py
@@ -1,6 +1,36 @@
 import json
 import sys
 
+# Returns the diff of two results
+def difference(x, y):
+    if "NAN" in x.upper() and "NAN" in y.upper(): # both are nan
+        return 0
+    
+    if "NAN" in x.upper() or "NAN" in y.upper():
+        return 1
+
+    if "INF" in x.upper() and "INF" in y.upper():
+        if x != y:
+            return 1
+        else:
+            return 0
+    
+    if "INF" in x.upper() or "INF" in y.upper():
+        return 1
+
+    # Assume the result is numerical
+    f1 = float(x)
+    f2 = float(y)
+
+    if (f1==0 and f2==0):
+        return 0
+
+    if (f1==0 or f2==0):
+        return abs(f1-f2)
+
+    ret = (f1-f2)/f1
+    return ret
+
 def main():
     fileName = sys.argv[1]
     
@@ -19,23 +49,28 @@ def main():
                             per_compiler_table[comp] = {d: result}
 
 
+    # Analysis for compiler pairs (c1, c2)
     allCompilers = list(per_compiler_table.keys())
     combinations  = set([])
     for i in range(len(allCompilers)):
-        c1 = allCompilers[i]
+        c1 = allCompilers[i] # compiler 1
         for j in range(i+1, len(allCompilers)):
-            c2 = allCompilers[j]
+            c2 = allCompilers[j] # compiler 2
             if c1 != c2:
+                #print("COMPILERS:", c1, c2)
                 diff = 0
                 allVals1 = per_compiler_table[c1]
                 allVals2 = per_compiler_table[c2]
                 for k in allVals1.keys():
-                    if allVals1[k] != allVals2[k]:
+                    resultC1 = allVals1[k]
+                    resultC2 = allVals2[k]
+                    #print("--->", resultC1, resultC2)
+                    if difference(resultC1, resultC2) != 0:
                         diff = diff + 1
-                        #print(c1, c2, i)
-                        if "O3" in k.split("-")[-1:]:
-                            print("\t-->", c1, c2, allVals1[k], allVals2[k])
-                            print(k)
+                        o = k.split("-")[-1:][0]
+                        prog = k.split("-")[0]
+                        if o == 'O0':
+                            print("\t", c1, c2, resultC1, resultC2, o, prog)
 
                 print("Diff:", c1, c2, diff)
                 
