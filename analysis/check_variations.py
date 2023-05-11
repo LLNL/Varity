018ff01fc8810261ce02b70d9ce79c40ab21a127
Ignacio Laguna <ilaguna@llnl.gov>
2023-05-11 08:17:47 -0700

    some plots

--- analysis/check_variations.py
+++ analysis/check_variations.py
@@ -1,6 +1,8 @@
 
 import sys
 import json
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 def loadData(fileName):
   with open(fileName) as f:
@@ -16,17 +18,28 @@ def loadData(fileName):
         for opt in data[prog][i][compiler]:
           result = data[prog][i][compiler][opt]
 
-          if opt == 'O3':
+          if opt == 'O2':
+            time = 0
             if "time:" in result:
               time = result.split()[1].split(':')[1]
-            else:
-              time = "0"
+              time = int(time)
 
-          if 'clang' in compiler: clang_results.append(time)
-          if 'g++' in compiler: gcc_results.append(time)
-          if 'xlc' in compiler: xlc_results.append(time)
-
-  print(clang_results)
+            if 'clang' in compiler: clang_results.append(time)
+            if 'gcc' in compiler: gcc_results.append(time)
+            if 'xlc' in compiler: xlc_results.append(time)
+  
+  x = list(range(1, len(clang_results)+1))
+  fig, ax = plt.subplots()
+  #ax.plot(x, clang_results, linestyle='.');
+  ax.scatter(x, clang_results, label='clang', marker='s');
+  ax.scatter(x, gcc_results, label='gcc', marker='P');
+  ax.scatter(x, xlc_results, label='xlc', marker='x');
+  ax.legend()
+  plt.yscale("log")
+  ax.set_ylabel('Time (usec)')
+  ax.set_xlabel('Runs')
+  plt.show()
+  #fig.savefig('parallel_programs.pdf', bbox_inches = "tight")
 
 if __name__ == '__main__':
   fileName = sys.argv[1]
